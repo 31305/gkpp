e6d484d08a6975d375da9b1afbd3bc55b01ec7e2
- <>
2025-10-25 11:58:51 +0000

    -

--- p.py
+++ p.py
@@ -2,6 +2,7 @@
 def vk(v):
     pass
 def pk(sn):
+    s=''
     k=0
     pk=0
     ps=[]
@@ -12,11 +13,17 @@ def pk(sn):
         p=sys.stdin.read(1)
         if ord(p)==4:break
         if p=='\n':
-            sp=True
-            pk+=1
-            if pk==len(ps[k]):
+            if sp:pk+=1
+            if pk==len(ps[k][1]):
                 k+=1
                 pk=0
+            if not sp:vk([71,44,4,49,1,55,53,7,51,2,75])
+            sp=True
+            vk(ps[k][1][pk])
+        elif ord(p)>=ord('0') and ord(p)<=ord('9'):
+            import time
+            s+=str(int(time.time()))+' '+':'.join([str(l) for l in ps[k][0]])+' '+p+'\n'
+
     curses.endwin()
     open(sn,'a').write('')
 if __name__=="__main__":
