2bce3f0454aea0fde18ec09602129043d4299faa
- <>
2025-10-28 07:29:07 +0000

    -

--- dv.py
+++ dv.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+gs=0
+gk=0
 def pk():
     from os import system
     import os.path
@@ -9,5 +11,10 @@ def pk():
         dn=ps[k][0]
         dn='"'+dn+'"'
         system('wget -O ds/'+str(k+1)+'.jpg '+dn)
+def pmk():
+    import p,sys
+    p.vk([51,2,66,7,53,2,65,6,77])
+    gs=int(sys.stdin.readline())
+    print(gs)
 if __name__=="__main__":
-    pk()
+    pmk()
